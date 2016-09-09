8b54450a78de70c11fab23eb3452d57c4f854763
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-09 18:21:49 +0300

    created TDD test1

---
+++ app/src/androidTest/java/ru/solandme/smarttipcalculator/MainActivityTest.java
@@ -0,0 +1,65 @@
+package ru.solandme.smarttipcalculator;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.SeekBar;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkLabels() {
+        onView(withId(R.id.tipLabelTextView)).check(matches(withText(R.string.tip)));
+    }
+
+    @Test
+    public void checkPercentBehavior(){
+        onView(withId(R.id.amountEditText)).perform(typeText("100"));
+        onView(withId(R.id.percentSeekBar)).perform(setProgress(5));
+        onView(withId(R.id.tipTextView)).check(matches(withText("10")));
+    }
+
+    private ViewAction setProgress(final int progress) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(SeekBar.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set progress on SeekBar";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                SeekBar seekBar = (SeekBar) view;
+                seekBar.setProgress(progress);
+            }
+        };
+    }
+
+
+}
